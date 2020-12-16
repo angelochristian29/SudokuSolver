29f01e783c3eb06ff537528dcc03c13d4e9150b0
angelochristian29 <71484821+angelochristian29@users.noreply.github.com>
2020-12-15 22:40:30 -0800

    Finalized GUI elements and added a "game over" message.

--- SudokuGUI.py
+++ SudokuGUI.py
@@ -1,6 +1,5 @@
 from SudokuSolver.SudokuBoard import SudokuBoard
 import sys, pygame as pg
-import time
 
 pg.init()
 font = pg.font.SysFont(None, 60)
@@ -20,6 +19,9 @@ class SudokuGUI:
             pg.draw.line(self.canvas, pg.Color("black"), pg.Vector2((i * 70) + 10, 10), pg.Vector2((i * 70) + 10, 640), line_width)
             pg.draw.line(self.canvas, pg.Color("black"), pg.Vector2(10, (i * 70) + 10), pg.Vector2(640, (i * 70) + 10), line_width)
             i += 1
+        if self.board_finished():
+            n_text = font.render("Game Over", True, pg.Color("black"))
+            self.canvas.blit(n_text, pg.Vector2(210, 680))
 
     def draw_nums(self, board):
         for r in range(9):
@@ -27,14 +29,13 @@ class SudokuGUI:
                 if board[r][c] != 0:
                     n_text = font.render(str(board[r][c]), True, pg.Color("black"))
                     self.canvas.blit(n_text, pg.Vector2((c * 70) + 33.5, (r * 70) + 27.5)) # modify this for sketching feature
-    def solve_draw(self, board, r, c, tof=True):
-        pg.draw.rect(self.canvas, pg.Color("white"), pg.Rect(c * 70 + 13.5, r * 70 + 13, 67.5, 67.5), 0)
-        n_text = font.render(str(board[r][c]), True, pg.Color("black"))
+
+    def solve_draw(self, board, r, c, color):
+        pg.draw.rect(self.canvas, pg.Color("white"), pg.Rect(c * 70 + 20, r * 70 + 20, 50, 50), 0)
+        n_text = font.render(str(board[r][c]), True, pg.Color(color))
         self.canvas.blit(n_text, pg.Vector2((c * 70) + 33.5, (r * 70) + 27.5))
-        if tof:
-            pg.draw.rect(self.canvas, pg.Color("orange"), pg.Rect(c * 70 + 13.5, r * 70 + 12, 67.5, 67.5), 3)
-        else:
-            pg.draw.rect(self.canvas, pg.Color("blue"), pg.Rect(c * 70 + 13.5, r * 70 + 12, 67.5, 67.5), 3)
+
+
     def backtracking_solver(self, board, r, c):
         if r > mySudoku.side - 1:
             return True
@@ -48,9 +49,10 @@ class SudokuGUI:
             for i in range(1, mySudoku.side + 1):
                 if mySudoku.is_present(board, r, c, i):
                     board[r][c] = i
-                    self.solve_draw(board, r, c, True)
+                    self.solve_draw(board, r, c, "blue")
                     pg.display.flip()
-                    pg.time.delay(50)
+                    pg.display.update()
+                    pg.time.delay(15)
                     if c == mySudoku.side - 1:
                         itr = self.backtracking_solver(board, r + 1, 0)
                     else:
@@ -58,13 +60,18 @@ class SudokuGUI:
                     if itr is True:
                         return True
             board[r][c] = 0
-            self.solve_draw(board, r, c, False)
+            self.solve_draw(board, r, c, "red")
             pg.display.flip()
-            pg.time.delay(50)
+            pg.display.update()
+            pg.time.delay(15)
             return False
+    def board_finished(self):
+        for r in range(mySudoku.side):
+            for c in range(mySudoku.side):
+                if mySudoku.s_board[r][c] == 0:
+                    return False
+        return True
 
-    def start_sol(self, board):
-        return self.backtracking_solver(board, 0, 0)
 
 # main
 def sudoku_loop():
@@ -73,17 +80,19 @@ def sudoku_loop():
     pg.display.set_caption("Sudoku Solver")
     to_run = True
     myGUI = SudokuGUI(canvas)
-    print("run")
     while to_run:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 to_run = False
             if event.type == pg.KEYDOWN:
-                if event.key == pg.K_1:
-                    myGUI.start_sol(mySudoku.s_board)
+                if event.key == pg.K_1 or event.key == pg.K_KP1:
+                    myGUI.backtracking_solver(mySudoku.s_board, 0, 0)
+                    if myGUI.board_finished():
+                        print("Game Over")
         myGUI.draw_canvas()
         myGUI.draw_nums(mySudoku.s_board)
         pg.display.flip()
 
 
 sudoku_loop()
+pg.quit()
